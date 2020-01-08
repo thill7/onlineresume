9149a0c3e77ba4f97818f74bfb9600c681a8e8aa
Tanner Hill <thill17@wou.edu>
2020-01-07 17:47:31 -0800

    toggle on experience page

--- client/src/components/experience.component.js
+++ client/src/components/experience.component.js
@@ -7,16 +7,36 @@ import Work from './work.component';
 export default class Experience extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            toggle: 0
+        };
+    }
+
+    onSetToggle(num) {
+        if(num != this.state.toggle) {
+            this.setState({toggle:num});
+        }
     }
 
     render() {
+        var {toggle} = this.state;
+
+        var noDisplay = {
+            display: "none"
+        };
+
         return(
             <div>
                 <MainNav page="1" />
+                <div className="experience-toggle d-flex flex-column flex-md-row">
+                    <button onClick={() => {this.onSetToggle(0);}} className={"r-subheading flex-fill"+(toggle == 0 ? " bg-secondary" : " bg-darker")}>Programming</button>
+                    <button onClick={() => {this.onSetToggle(1);}} className={"r-subheading flex-fill"+(toggle == 1 ? " bg-secondary" : " bg-darker")}>Work History</button>
+                </div>
                 <div className="container">
-                    <Languages />
+                    <Languages style={toggle == 1 ? noDisplay : {}} />
                     <hr />
-                    <Work />
+                    <Work style={toggle == 0 ? noDisplay : {}} />
                 </div>
             </div>
         )
