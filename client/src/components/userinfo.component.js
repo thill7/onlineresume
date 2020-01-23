355a4958b7abbad6162fb4d46db567c821680fa0
Tanner Hill <thill17@wou.edu>
2020-01-23 12:36:24 -0800

    changed info in userinfo component

--- client/src/components/userinfo.component.js
+++ client/src/components/userinfo.component.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSync} from '@fortawesome/free-solid-svg-icons';
+import {faSync, faCheckCircle, faWindowClose} from '@fortawesome/free-solid-svg-icons';
 
 export default class UserInfo extends Component {
     constructor(props) {
@@ -29,8 +29,9 @@ export default class UserInfo extends Component {
                 <div className="media p-3 bg-primary m-4 text-white rounded">
                     <img src={userObject.avatar_url} alt={userObject.login} className="mr-3 rounded-circle img-github" />
                     <div className="media-body">
-                    <h4 className="r-subheading"><span className="font-weight-bold">Github: </span><a href={userObject.login}></a></h4>
+                    <h4 className="r-subheading"><span className="font-weight-bold">Github</span>: <a href={userObject.html_url}>{userObject.login}</a></h4>
                     <p className="r-text-small"><span className="font-weight-bold">Public Repos</span>: {userObject.public_repos}</p>
+                    <p className="r-text-small"><span className="font-weight-bold">Hireable</span>: <FontAwesomeIcon icon={userObject.hireable ? faCheckCircle : faWindowClose} /></p>
                     </div>
                 </div>
                 
